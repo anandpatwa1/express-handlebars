99129740c06aed4c1553dac9a52c16bbea4cd6ce
anandpatwa1 <anandpatwa.js@gmail.com>
2023-09-25 01:13:22 +0530

    1.1

--- index.js
+++ index.js
@@ -4,16 +4,34 @@ const PORT = 4000;
 const path = require('path');
 
 const staticWeb = path.join(__dirname, './public');
+const templet = path.join(__dirname, './templet');
+
 // to set the vue engine
 app.set('view engine', 'hbs');
+// if you  change name of "views" File
+app.set('views', templet);
 
 // app.use(express.static(staticWeb));
 
 // templet engine route
-app.use('/', (req, res) => {
-    res.render('index',{
-        text : "i am from index.js file"
+app.get('/', (req, res) => {
+  res.render('index', {
+    text: 'i am from index.js file',
+  });
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/about/*', (req, res) => {
+    res.render('error', {
+        text: 'dont found anything inside about us page',
     });
+});
+
+app.get('*', (req, res) => {
+  res.render('error', {
+    text: 'note found this page',
+  });
 });
 app.use('/', (req, res) => {
   res.send('hello everyone');
